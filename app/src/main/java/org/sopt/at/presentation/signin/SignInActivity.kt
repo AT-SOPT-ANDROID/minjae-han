be2c0f506ae88dea1f4bf11b5b18a666fc264c80
MinJae Han <hanminjae131@gmail.com>
2025-04-06 23:41:47 +0900

    [MOD/#1] Add SignUp navigation and adjust system UI
    This commit introduces navigation to the SignUp screen and adjusts the system UI appearance in the SignInActivity.
    
    Key changes:
    
    -   Added navigation from `SignInActivity` to `SignUpActivity`.
    -   Modified `SignInRoute` to handle `onBackClick` and `onSignUpClick` callbacks for navigation.
    -   Set the `darkTheme` property to `true` in `ATSOPTANDROIDTheme`.
    - Set the `isAppearanceLightNavigationBars` and `isAppearanceLightStatusBars` to false.
    -   Enabled edge-to-edge display in `SignInActivity` using `enableEdgeToEdge()`.
    - Added  `SuppressLint("SourceLockedOrientationActivity")` annotation.
    - Added WindowCompat.setDecorFitsSystemWindows.

--- app/src/main/java/org/sopt/at/presentation/signin/SignInActivity.kt
+++ app/src/main/java/org/sopt/at/presentation/signin/SignInActivity.kt
@@ -1,5 +1,7 @@
 package org.sopt.at.presentation.signin
 
+import android.annotation.SuppressLint
+import android.app.Activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -7,17 +9,40 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
-import org.sopt.at.presentation.signin.component.SignInRoute
+import androidx.compose.ui.platform.LocalContext
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsControllerCompat
+import org.sopt.at.presentation.signup.SignUpActivity
 import org.sopt.at.ui.theme.ATSOPTANDROIDTheme
 
 class SignInActivity : ComponentActivity() {
+    @SuppressLint("SourceLockedOrientationActivity")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        WindowInsetsControllerCompat(window, window.decorView).apply {
+            this.isAppearanceLightNavigationBars = false
+            this.isAppearanceLightStatusBars = false
+        }
         setContent {
-            ATSOPTANDROIDTheme {
+            ATSOPTANDROIDTheme(
+                darkTheme = true
+            ) {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    SignInRoute(paddingValues = innerPadding)
+                    val context = LocalContext.current
+                    SignInRoute(
+                        paddingValues = innerPadding,
+                        onBackClick = { (context as? Activity)?.onBackPressed() },
+                        onSignUpClick = {
+                            val intent = android.content.Intent(
+                                context,
+                                SignUpActivity::class.java
+                            )
+                            context.startActivity(intent)
+                        }
+                    )
                 }
             }
         }
