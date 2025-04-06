fc424b9ff2080e43d83c2262c960e21a48c5648c
MinJae Han <hanminjae131@gmail.com>
2025-04-06 23:42:04 +0900

    [MOD/#1] Move SignInRoute to the correct package
    This commit moves the `SignInRoute` composable from the `org.sopt.at.presentation.signin.component` package to `org.sopt.at.presentation.signin`, aligning it with the correct file structure. Additionally, added `onBackClick` and `onSignUpClick` lambda parameters and removed `Context` from composable function.

--- app/src/main/java/org/sopt/at/presentation/signin/SignInRoute.kt
+++ app/src/main/java/org/sopt/at/presentation/signin/SignInRoute.kt
@@ -1,4 +1,4 @@
-package org.sopt.at.presentation.signin.component
+package org.sopt.at.presentation.signin
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -18,6 +18,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
@@ -31,27 +32,27 @@ import org.sopt.at.core.designsystem.component.button.BasicButton
 import org.sopt.at.core.designsystem.component.textfield.PasswordTextField
 import org.sopt.at.core.designsystem.component.textfield.UserIdTextField
 import org.sopt.at.core.designsystem.component.topbar.BasicTopBar
+import org.sopt.at.presentation.signin.component.MenuList
 import org.sopt.at.ui.theme.ATSOPTANDROIDTheme
 
 @Composable
 fun SignInRoute(
     paddingValues: PaddingValues,
+    onBackClick: () -> Unit,
+    onSignUpClick: () -> Unit,
     modifier: Modifier = Modifier
 ) {
     var userId by remember { mutableStateOf("") }
     var userPassword by remember { mutableStateOf("") }
-    val context = LocalContext.current
 
     SignInScreen(
-        onBackClick = {
-            (context as? android.app.Activity)?.finish()
-        },
+        onBackClick = onBackClick,
         userId = userId,
         onUserIdChanged = { userId = it },
         userPassword = userPassword,
         onPasswordChanged = { userPassword = it },
         onSignInClick = { },
-        onSignUpClick = { },
+        onSignUpClick = onSignUpClick,
         modifier = modifier.padding(paddingValues)
     )
 }
