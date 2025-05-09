32ef99ed6d3ef80edf5335eb636995e534688909
MinJae Han <hanminjae131@gmail.com>
2025-05-10 00:40:44 +0900

    [MOD/#7] Dto miner

---
+++ app/src/main/java/org/sopt/at/data/dto/request/SignUpRequestDto.kt
@@ -0,0 +1,15 @@
+package org.sopt.at.data.dto.request
+
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class SignUpRequestDto(
+    @SerialName("loginId")
+    val loginId: String,
+    @SerialName("nickname")
+    val nickname: String,
+    @SerialName("password")
+    val password: String
+)
